330bf143675163246981e9f97106287fe935b405
Rudra <105686193+pandarudra@users.noreply.github.com>
2025-01-07 06:34:27 +0530

    Time: 3 ms (48.52%) | Memory: 12.2 MB (17.90%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
@@ -2,8 +2,14 @@ class Solution {
 public:
     vector<string> stringMatching(vector<string>& words) {
         vector<string> res;
-        for(auto it:words){
-            for(auto match:words){
+        int n=words.size();
+        sort(words.begin(),words.end(),[](const string& a,const string& b){
+            return a.length()<b.length();
+        });
+        for(int i=0;i<n;i++){
+            string it=words[i];
+            for(int j=i+1;j<n;j++){
+                string match=words[j];
                 if(it!=match){
                     auto present=match.find(it);
                     if(present!=string::npos){
