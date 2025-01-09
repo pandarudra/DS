0105c2651de4c9bf5d3295a14fae7b9d9e0fb8fc
Rudra <105686193+pandarudra@users.noreply.github.com>
2025-01-09 11:10:43 +0530

    Time: 55 ms (5.04%) | Memory: 111.1 MB (5.04%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.cpp
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.cpp
@@ -1,12 +1,62 @@
+struct trie{
+    trie* links[26];
+    bool end =  false;
+
+    bool contains(char ch){
+        return links[ ch - 'a' ] != nullptr;
+    }
+
+    trie* gofurther(char ch){
+        return links[ ch - 'a' ];
+    }
+
+    void put(char ch){
+        links[ ch - 'a' ] = new trie();
+    }
+};
 class Solution {
+private:
+    trie* root;
+    
+    
+
+    void insert(string  word){
+        trie* dummy = root;
+
+        for(int i = 0; i < word.length() ; i++){
+            if(!dummy->contains(word[i])){
+                dummy->put(word[i]);
+            }
+            dummy = dummy->gofurther(word[i]);
+        } 
+
+        dummy->end = true;
+
+    }
+
+    bool search(string prefix){
+        trie* dummy = root;
+        for(int i = 0; i < prefix.length() ; i++){
+            if(dummy->contains(prefix[i])){
+                dummy = dummy->gofurther(prefix[i]);
+            }else{
+                root = new trie();
+                return false;
+            }
+        }
+        root = new trie();
+        return true;
+    }
 public:
+    Solution(){
+        root = new trie();
+    }
+
     int prefixCount(vector<string>& words, string pref) {
-        int len=pref.length();
-        int n=words.size();
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            string prefix=words[i].substr(0,len);
-            if(pref==prefix){
+        int cnt = 0;
+        for( auto &str : words ){
+            insert(str);
+            if(search(pref)){
                 cnt++;
             }
         }
