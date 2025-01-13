c80ff3b3ea1ca4f320c2be253dd208bb24240969
Rudra <105686193+pandarudra@users.noreply.github.com>
2025-01-13 17:27:23 +0530

    Time: 15 ms (67.34%) | Memory: 32.1 MB (74.19%) - LeetSync

---
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        int cnt   = 0 ;
+        int freq[26] = {0} ;
+        for(int i = 0 ; i < s.length() ; i++){
+            freq[s[i] - 'a']++ ;
+        }
+        for(auto &it  : freq){
+            if(it != 0){
+                if(it & 1){
+                    cnt += 1 ;
+                }else{
+                    cnt += 2 ;
+                }
+            }
+        }
+        return cnt ; 
+    }
+};
