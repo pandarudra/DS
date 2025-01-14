40e1b1e2ed2a98a62ba2602d2591a2eee795ecf9
Rudra <105686193+pandarudra@users.noreply.github.com>
2025-01-14 09:47:08 +0530

    Time: 10 ms (47.73%) | Memory: 88 MB (52.27%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
@@ -1,23 +1,26 @@
 #define uset unordered_set
+#define umap unordered_map
 class Solution {
 public:
-    int cntEquals(uset<int>& a , uset<int>& b){
-        int  cnt = 0 ;
-        for(auto &it : a){
-            if(b.count(it)){
-                cnt++ ;
-            }
-        }
-        return  cnt ;
-    }
+    
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-        uset<int> sA , sB ;
+        umap<int , int> mp ;
         int n = A.size() ;
         vector<int> prefixCnt(n , 0) ;
+        int cnt = 0 ;
         for(int i = 0 ; i < n  ; i++){
-            sA.insert(A[i]) ;
-            sB.insert(B[i]) ;
-            prefixCnt[i] = cntEquals(sA , sB) ;
+            mp[A[i]]++ ;
+            mp[B[i]]++;
+            
+            if(A[i] != B[i]){
+                if(mp[A[i]] > 1)cnt++ ;
+                if(mp[B[i]] > 1)cnt++ ;
+            }else{
+                cnt++ ;
+            }
+            
+
+            prefixCnt[i] = cnt ;
         }
         return prefixCnt ;
     }
