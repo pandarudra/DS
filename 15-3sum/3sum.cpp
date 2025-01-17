6ebaabf23bf34eaae2e8f9ecf29f591154e4b8cd
Rudra <105686193+pandarudra@users.noreply.github.com>
2025-01-17 09:17:43 +0530

    Time: 57 ms (38.54%) | Memory: 31 MB (36.53%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,39 +1,29 @@
 #define all(v) (v).begin(),(v).end()
 #define uset unordered_set
 
-
-struct VectorHash {
-    size_t operator()(const vector<int>& vec) const {
-        size_t hash = 0;
-        for (int num : vec) {
-            hash ^= hash * 31 + std::hash<int>()(num);
-        }
-        return hash;
-    }
-};
-
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(all(nums));
         int n = nums.size() ;
-        uset<vector<int>,VectorHash> container ;
+        
         vector<vector<int>> sol ;
         for(int i = 0 ; i < n ; i++){
+
+            if(i > 0 && nums[i] == nums[i-1])continue ;
+
             int a = -nums[i] ;
             int low = i + 1 , high = n - 1 ;
             while(low < high){
                 if(nums[low] + nums[high] == a){
                     vector<int> v = {nums[i],nums[low],nums[high]} ;
-                    sort(all(v)) ;
-                    if(!container.count(v)){
-                        sol.push_back(v) ;
-                        container.insert(v) ;
-                    }
+                    sol.push_back(v) ;
+
+                    while(low < high && nums[low] == nums[low + 1])low ++ ;
+                    while(low < high  && nums[high] == nums[high - 1])high -- ;
                     low ++ ;
                     high -- ;
-                }
-                if(nums[low] + nums[high] > a){
+                }else if(nums[low] + nums[high] > a){
                     high -- ;
                 }else if(nums[low] + nums[high] < a){
                     low ++ ;
