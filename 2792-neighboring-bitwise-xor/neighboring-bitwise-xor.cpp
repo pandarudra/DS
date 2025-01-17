1d2316f79992d745c3bca5d86a84f3a4b88516c4
Rudra <105686193+pandarudra@users.noreply.github.com>
2025-01-17 07:55:54 +0530

    Time: 12 ms (18.69%) | Memory: 264 MB (48.13%) - LeetSync

---
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool doesValidArrayExist(vector<int>& derived) {
+        int n = derived.size() ;
+        if(n == 1){
+            if(derived[0] == 0){
+                return true ;
+            }else{
+                return false ;
+            }
+        }
+        int cntZero = 0 ;
+        for(auto &it : derived){
+            if(it == 0){
+                cntZero ++ ;
+            }
+        }
+        if(cntZero == n)return true ;
+        if(n % 2 == 0 && cntZero % 2 == 0)return true ;
+        if(n % 2 != 0 && cntZero % 2 != 0)return true ;
+        if(cntZero == 0){
+            if(n % 2 == 0)return true ;
+            else return false ;
+        }
+        return false ;
+    }
+};
