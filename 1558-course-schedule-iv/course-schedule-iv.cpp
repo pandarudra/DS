5c597f8d8455b2e6324c44660934486ce531dc92
Rudra <105686193+pandarudra@users.noreply.github.com>
2025-01-27 08:30:58 +0530

    Time: 17 ms (85.33%) | Memory: 70.2 MB (58.96%) - LeetSync

---
+++ 1558-course-schedule-iv/course-schedule-iv.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void bfs(vector<vector<int>>& graph , vector<vector<int>>& canReach , vector<int> vis , int i){
+        queue<int> q ;
+        vis[i] = 1 ;
+        q.push(i) ;
+        while(!q.empty()){
+            int  node = q.front() ;
+            q.pop() ;
+            for(auto &it : graph[node]){
+                if(!vis[it]){
+                    vis[it] = 1 ;
+                    canReach[i][it] = 1 ;
+                    q.push(it) ;
+                }
+            }
+        }
+    }
+    vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
+        vector<vector<int>> graph(numCourses) ;
+        for(auto &it : prerequisites){
+            graph[it[0]].push_back(it[1]) ;
+        }
+        vector<int> vis(numCourses , 0) ;
+        vector<vector<int>> canReach(numCourses , vector<int>(numCourses , 0)) ;
+        
+        for(int i = 0 ; i < numCourses ; i++){
+            bfs(graph , canReach , vis , i) ;
+        }
+        vector<bool> res ;
+        for(auto &q : queries){
+            bool x = (canReach[q[0]][q[1]] == 1) ? true : false ;
+            res.push_back(x) ;
+        }
+        return res ;
+    }
+};
