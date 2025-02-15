ca68e4896553979d326529f19ed26949f20c07c8
Rudra <105686193+pandarudra@users.noreply.github.com>
2025-02-15 09:34:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (80.60%) - LeetSync

--- 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
@@ -1,20 +1,7 @@
 class Solution {
 private:
-    // bool sumD(int n , int sq){
-        
-    // }
     vector<int> precompute = {1,9,10,36,45,55,82,91,99,100,235,297,369,370,379,414,657,675,703,756,792,909,918,945,964,990,991,999,1000};
-    // void calc(){
-    //     for(int i = 1 ; i <= 1000 ; i++){
-    //         if(sumD(i , i * i)){
-    //             precompute.push_back(i) ;
-    //         }
-    //     }
-    // }
-public:
-    // Solution(){
-    //     calc() ;
-    // }
+    public:
     int punishmentNumber(int n) {
         int  s = 0 ;
         for(int i  = 0 ; i < precompute.size() ; i++){
