2ae14fb17c2b99debe168ad47e6a172c17cde17b
Rudra <105686193+pandarudra@users.noreply.github.com>
2025-02-26 16:27:39 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (51.09%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
@@ -4,26 +4,23 @@ public:
         int n = nums.size() ;
 
         int mxKadan = INT_MIN ;
-        int s = 0 ;
-
-        for(int i = 0 ; i < n ; i++){
-            if(s < 0){
-                s = 0 ;
-            }
-            s += nums[i] ;
-            mxKadan = max(mxKadan , s) ;
-        }
-
         int mnKadan = INT_MAX ;
-        s = 0 ;
+        int s1 = 0 , s2 = 0 ;
 
         for(int i = 0 ; i < n ; i++){
-            if(s > 0){
-                s = 0 ;
+            if(s1 < 0){
+                s1 = 0 ;
             }
-            s += nums[i] ;
-            mnKadan = min(mnKadan , s) ;
+            if(s2 > 0){
+                s2 = 0 ;
+            }
+            s1 += nums[i] ;
+            mxKadan = max(mxKadan , s1) ;
+
+            s2 += nums[i] ;
+            mnKadan = min(mnKadan , s2) ;
         }
+
 
         return max(abs(mxKadan) , abs(mnKadan)) ;
     }
