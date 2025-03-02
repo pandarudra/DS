d77215d05917df12c76bdfc42c04b8d8eff7af7f
Rudra <105686193+pandarudra@users.noreply.github.com>
2025-03-02 20:29:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (6.24%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,27 @@
+#define ll long long 
+class Solution {
+public:
+    int reverse(int x) {
+        bool sign = false ;
+        ll y = x ;
+        if(y < 0){
+            sign = true ;
+            y = abs(y) ;
+        }
+
+        ll rev = 0 ;
+        while(y > 0){
+            rev = rev * 10 + (y % 10) ;
+            y /= 10 ; 
+        }
+
+        if(sign){
+            rev = -rev ;
+        }
+
+        if(rev < INT_MIN || rev > INT_MAX)return 0 ;
+       
+
+        return (int)rev ;
+    }
+};
