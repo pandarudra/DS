6e1422df5ffb45a0880b006abbd5041047626250
Rudra <105686193+pandarudra@users.noreply.github.com>
2025-03-04 23:03:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (19.48%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,14 +1,30 @@
+#include <unordered_set>
+
 #define uset unordered_set
+
 class Solution {
 public:
     bool checkPowersOfThree(int n) {
+        uset<int> s;
         
-        while(n > 0){
-            if(n % 3 == 2){
-                return false ;
+        while (n > 0) {
+            int power = 1, exponent = 0;
+            
+            // Find the largest power of 3 that is ≤ n using integer arithmetic
+            while (power * 3 <= n) {
+                power *= 3;
+                exponent++;
             }
-            n /= 3 ;
+            
+            // If the exponent is already used, return false
+            if (s.count(exponent)) {
+                return false;
+            }
+            
+            s.insert(exponent);
+            n -= power;  // Subtract the found power from n
         }
-        return true ;
+        
+        return true;
     }
 };
