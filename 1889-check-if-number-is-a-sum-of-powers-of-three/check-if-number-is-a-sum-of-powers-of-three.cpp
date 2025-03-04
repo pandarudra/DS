e5bb3eac87d2a058c1070ce6727c9927afe43c40
Rudra <105686193+pandarudra@users.noreply.github.com>
2025-03-04 23:06:32 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (19.48%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,30 +1,22 @@
-#include <unordered_set>
-
 #define uset unordered_set
-
 class Solution {
 public:
     bool checkPowersOfThree(int n) {
-        uset<int> s;
-        
-        while (n > 0) {
-            int power = 1, exponent = 0;
-            
-            // Find the largest power of 3 that is ≤ n using integer arithmetic
-            while (power * 3 <= n) {
-                power *= 3;
-                exponent++;
+        uset<int> s ;
+        while(n > 0){
+            int mxpow = 0 , pvalue = 1 ;
+
+            while(pvalue * 3 <= n){
+                pvalue *= 3 ;
+                mxpow++ ;
             }
-            
-            // If the exponent is already used, return false
-            if (s.count(exponent)) {
-                return false;
+
+            if(s.count(mxpow)){
+                return false ;
             }
-            
-            s.insert(exponent);
-            n -= power;  // Subtract the found power from n
+            s.insert(mxpow) ;
+            n -= pvalue ;
         }
-        
-        return true;
+        return true ;
     }
 };
