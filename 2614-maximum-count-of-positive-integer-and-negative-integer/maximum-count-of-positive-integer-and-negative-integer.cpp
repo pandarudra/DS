2094720fd208804e151345395feca4deb0456d82
Rudra <105686193+pandarudra@users.noreply.github.com>
2025-03-12 20:22:36 +0530

    Time: 0 ms (100.00%) | Memory: 21.3 MB (75.85%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,36 +1,13 @@
+#define lb lower_bound
+#define ub upper_bound
+#define all(v) (v).begin(),(v).end()
+
 class Solution {
-private:
-    int beforeZ(vector<int>& nums){
-        int n = nums.size() ;
-        int low = 0 , high = n - 1 ;
-        while(low <= high){
-            int mid = low + (high - low) / 2 ;
-            if(nums[mid] < 0){
-                low = mid + 1 ;
-            }else{
-                high = mid - 1 ;
-            }
-        }
-        return high ;  
-    }
-    int afterZ(vector<int>& nums){
-        int n = nums.size() ;
-        int low = 0 , high = n - 1 ;
-        while(low <= high){
-            int mid = low + (high - low) / 2 ;
-            if(nums[mid] <= 0){
-                low = mid + 1 ;
-            }else{
-                high = mid - 1 ;
-            }
-        }
-        return low ;  
-    }
 public:
     int maximumCount(vector<int>& nums) {
         int n = nums.size() ;
-        int h = beforeZ(nums) ;
-        int h_ = afterZ(nums) ;
-        return max(h + 1 , n - h_) ;
+        int h = lb(all(nums) , 0) - nums.begin() ;
+        int h_ = ub(all(nums) , 0) - nums.begin() ;
+        return max(h , n - h_) ;
     }
 };
