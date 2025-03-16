fb2a8794b69fe3abd30a0fc115fa029c262f3b75
Rudra <105686193+pandarudra@users.noreply.github.com>
2025-03-16 07:13:40 +0530

    Time: 63 ms (31.07%) | Memory: 59.2 MB (74.95%) - LeetSync

---
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
@@ -0,0 +1,27 @@
+#define all(v) (v).begin(),(v).end()
+#define ll long long
+class Solution {
+private:
+    bool canRepair(vector<int>& ranks , ll mid , int cars){
+        ll can = 0 ;
+        for(auto& it : ranks){
+            can += (sqrt(mid * (1.0) / it)) ;
+        }
+        return can >= cars ;
+    }
+public:
+    long long repairCars(vector<int>& ranks, int cars) {
+        ll low = 1LL , high = 1000000000000000LL ;
+
+        while(low <= high){
+            ll mid = low + (high - low) / 2 ;
+            if(canRepair(ranks , mid , cars)){
+                high = mid - 1 ;
+            }else{
+                low = mid + 1 ;
+            }
+        }
+
+        return low ;
+    }
+};
