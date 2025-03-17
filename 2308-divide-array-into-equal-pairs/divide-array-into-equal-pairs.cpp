147d735dbf846fee86043052dde0cc73eb5224b3
Rudra <105686193+pandarudra@users.noreply.github.com>
2025-03-17 16:00:17 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (61.67%) - LeetSync

---
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        int freq[1002] = {} ;
+        for(auto& num : nums){
+            freq[num]++ ;
+        }
+        for(auto& it : freq){
+            if(it & 1){
+                return false ;
+            }
+        }
+        return true ;
+    }
+};
