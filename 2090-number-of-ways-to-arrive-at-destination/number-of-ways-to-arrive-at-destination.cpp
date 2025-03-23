b75a82c45aad576f4679fdc27d8be228cb9a773f
Rudra <105686193+pandarudra@users.noreply.github.com>
2025-03-23 20:47:48 +0530

    Time: 1 ms (95.29%) | Memory: 37.7 MB (43.81%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,46 +1,36 @@
 #define ll long long 
 #define pll pair<ll , ll>
 class Solution {
-private:
     const ll mod = 1e9 + 7 ;
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        
         vector<pll> adj[n] ;
-        // wt - node 
-
-        for(auto &it : roads){
-            ll u = (ll)it[0] ;
-            ll v = (ll)it[1] ;
-            ll t = (ll)it[2] ;
-
+        for(auto& rd : roads){
+            ll u = (ll) rd[0] ;
+            ll v = (ll) rd[1] ;
+            ll t = (ll) rd[2] ;
             adj[u].push_back({v , t}) ;
             adj[v].push_back({u , t}) ;
         }
 
-        vector<ll> dist(n , 1e12) , ways(n , 0) ;
-        
-        dist[0] = 0 ;
+        vector<ll> time(n , 1e12) , ways(n , 0) ;
+        time[0] = 0 ;
         ways[0] = 1 ;
-
-        priority_queue<pll , vector<pll> , greater<pll>> pq ;
+        priority_queue <pll , vector<pll> , greater<pll>> pq ;
         pq.push({0 , 0}) ;
-
         while(!pq.empty()){
-            auto [d , u] = pq.top() ;
+            auto [t , node] = pq.top() ;
             pq.pop() ;
-
-            for(auto& [v , p] : adj[u]){
-                if(p + d < dist[v]){
-                    dist[v] = p + d ;
-                    ways[v] = ways[u] % mod ;
-                    pq.push({p + d , v}) ;
-                }else if(p + d == dist[v]){
-                    ways[v] = (ways[v] + ways[u]) % mod ;
+            for(auto& [node_ , t_] : adj[node]){
+                if(t + t_ < time[node_]){
+                    time[node_] = t + t_ ;
+                    ways[node_] = ways[node] % mod ;
+                    pq.push({t + t_ , node_}) ;
+                }else if(t + t_ == time[node_]){
+                    ways[node_] = (ways[node] + ways[node_]) % mod ;
                 }
             }
-        } 
-
+        }
         return (int) ways[n - 1] ;
     }
 };
