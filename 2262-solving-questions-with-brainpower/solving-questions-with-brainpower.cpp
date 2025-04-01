8130ab063ce4af8a55b90f7616ac3a8276ec4aa1
Rudra <105686193+pandarudra@users.noreply.github.com>
2025-04-01 10:26:54 +0530

    Time: 9 ms (39.19%) | Memory: 127.6 MB (16.19%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -1,18 +1,19 @@
 #define  ll long long
 class Solution {
+private:
+    ll pnp(vector<vector<int>>& q , ll i , vector<ll>& dp , ll n){
+        if(i >= n) return 0;
+        if(dp[i] != -1) return dp[i] ;
+
+        ll take = q[i][0] + pnp(q , i + q[i][1] + 1 , dp ,  n) ;
+        ll not_take = pnp(q , i + 1 , dp ,  n) ;
+
+        return dp[i] = max(take , not_take) ;
+    }
 public:
     long long mostPoints(vector<vector<int>>& questions) {
         ll n = questions.size() ;
-        vector<ll> dp(n + 1 , 0) ;
-        for(ll i = n - 1 ; i >= 0 ; i--){
-            ll pt = questions[i][0] ;
-            ll bp = questions[i][1] ;
-
-            ll take = pt + ((bp + i + 1) < n ? dp[bp + i + 1] : 0) ;
-            ll not_take = dp[i + 1] ;
-
-            dp[i] = max(take , not_take) ;
-        }
-        return dp[0] ;
+        vector<ll> dp(n , -1) ;
+        return pnp(questions , 0 , dp , n) ;
     }
 };
