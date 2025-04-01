eda0881e1c15af69ffbe86c0f6d0a828490921ae
Rudra <105686193+pandarudra@users.noreply.github.com>
2025-04-01 22:53:13 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (38.97%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -5,8 +5,8 @@ public:
         dp[0] = 0 ;
         for(int i = 1 ; i <= n ; i++){
             int h_bit = log2(i) ;
-            int p = pow(2 , h_bit) ;
-            dp[i] = 1 + dp[i - p] ; 
+            int maxPowOf2 = pow(2 , h_bit) ;
+            dp[i] = 1 + dp[i - maxPowOf2] ; 
         }
         return dp ;
     }
