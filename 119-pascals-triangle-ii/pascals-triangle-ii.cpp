f159dcb481745b91ef4de86b94af81744d54be8a
Rudra <105686193+pandarudra@users.noreply.github.com>
2025-04-01 17:06:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (75.79%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -1,15 +1,25 @@
+#define ll long long
 class Solution {
+private:
+    int ncr(int n , int i){
+        ll res = 1 ;
+        for(int j = 1 ; j <= i ; j++){
+            res *= (n--) ;
+            res /= j ;
+        }
+        return (int) res ;
+    }
 public:
     vector<int> getRow(int rowIndex) {
-        vector<vector<int>> dp(rowIndex + 1) ;
-        dp[0] = {1} ;
-        for(int i = 1 ; i <= rowIndex ; i++){
-            dp[i].resize(i + 1) ;
-            dp[i][0] = dp[i][i] = 1 ;
-            for(int j = 1 ; j < i ; j++){
-                dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1] ;
+        vector<int> dp(rowIndex + 1 , -1) ;
+        dp[0] = dp[rowIndex] = 1 ;
+        for(int i = 1 ; i < rowIndex ; i++){
+            if(dp[rowIndex - i] != -1){
+                dp[i] = dp[rowIndex - i] ;
+            }else{
+                dp[i] = ncr(rowIndex , i) ;
             }
         }
-        return dp[rowIndex] ;
+        return dp ;
     }
 };
