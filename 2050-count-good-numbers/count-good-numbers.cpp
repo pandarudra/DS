c23e2ec3ca00a411531b679b87e6ec5fbbea3e15
Rudra <105686193+pandarudra@users.noreply.github.com>
2025-04-13 20:22:11 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (3.22%) - LeetSync

---
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -0,0 +1,24 @@
+#define ll long long 
+class Solution {
+private:
+    const ll MOD = 1e9 + 7 ;
+    ll mpow(ll x , ll n){
+        if(n == 0){
+            return 1LL ;
+        }
+        ll half = mpow(x , n / 2)  ;
+        if(n & 1){
+            return (x * half * half) % MOD ;
+        }
+        return (half * half) % MOD ;
+    }
+public:
+    int countGoodNumbers(long long n) {
+        ll ne = ceil((1.0) * n / 2) ;
+        ll no = n / 2 ;
+        ll res = 1LL ;
+        res = mpow(5 , ne) ;
+        res = (res * mpow(4 , no)) % MOD ;
+        return res ;
+    }
+};
