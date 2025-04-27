5364ebf213b967535d7a6ed8aaf79cfdb0a9a905
Rudra <105686193+pandarudra@users.noreply.github.com>
2025-04-27 20:28:42 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (70.08%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -2,25 +2,22 @@
 class Solution {
 private:
     umap<int , int> memo ;
-    int dp(vector<int>& nums , int n , int k){
-        if(n == 0){
-            return 0 ;
-        }
-        if(memo.count(n)){
-            return memo[n] ;
-        }
-        int mx = 0 ;
+    int fn(vector<int>& nums , int n , int k){
+        if(n == 0) return 0 ;
+        if(memo.count(n)) return memo[n] ;
+        int s = 0 ;
         for(int i = 2 ; i < k ; i++){
-            int left = (n - i >= 0) ? dp(nums , n - i , k) + nums[n - i] : 0 ;
-            mx = max(mx , left) ;
+            int left = (n - i >= 0) ? fn(nums , n - i , k) + nums[n - i] : 0 ;
+            s = max(s , left) ;
         }
-        return memo[n] = mx ;
+        return memo[n] = s ;
     }
 public:
     int rob(vector<int>& nums) {
         int n = nums.size() ;
-        int left = dp(nums , n - 1 , n) + nums[n - 1];
-        int right = (n - 2 >= 0) ? dp(nums , n - 2 , n) + nums[n - 2] : INT_MIN ;
+        int left = fn(nums , n - 1 , n) ;
+        left = (left > 0) ? left + nums[n - 1] : nums[n - 1] ;
+        int right = (n - 2 >= 0) ? fn(nums , n - 2 , n) + nums[n - 2] : INT_MIN ;
         return max(left , right) ;
     }
 };
