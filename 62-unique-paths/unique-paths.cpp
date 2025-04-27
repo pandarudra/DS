dceeffc5559f65ba6b9b088e63583dc5f6b13f7d
Rudra <105686193+pandarudra@users.noreply.github.com>
2025-04-28 01:26:22 +0530

    Time: 2 ms (18.53%) | Memory: 9.4 MB (32.93%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,19 +1,19 @@
-#define ipair pair<int , int>
 class Solution {
 private:
-    map<ipair , int> memo ;
+    vector<vector<int>> memo ;
     int f(int m , int n){
         if(m == 0 && n == 0) return 1 ;
         if(m < 0 || n < 0) return 0 ;
 
-        if(memo.count({m , n})) return memo[{m , n}] ;
-        
+        if(memo[m][n] != -1) return memo[m][n] ;
+
         int left = f(m - 1 , n) ;
         int right = f(m , n - 1) ;
-        return memo[{m , n}] = left + right ;
+        return memo[m][n] = left + right ;
     }
 public:
     int uniquePaths(int m, int n) {
+        memo.assign(m , vector<int>(n , -1)) ;
         return f(m - 1 , n - 1) ;
     }
 };
