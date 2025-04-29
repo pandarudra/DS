3de77a1eae3b128671c56fb2e8bd48d0e2e9cc20
Rudra <105686193+pandarudra@users.noreply.github.com>
2025-04-29 08:45:09 +0530

    Time: 15 ms (4.17%) | Memory: 14.9 MB (5.50%) - LeetSync

---
+++ 120-triangle/triangle.cpp
@@ -0,0 +1,25 @@
+#define ll long  long
+#define ipair pair<int , int>
+class Solution {
+private:
+    map<ipair , int> memo ;
+    ll f(vector<vector<int>>& t , int m , int n){
+        if(m == 0 && n == 0) return t[0][0] ;
+
+        if(memo.count({m , n})) return memo[{m , n}] ;
+
+        ll top = (m - 1 >= 0 && n <= m) ?(ll) t[m][n] + f(t , m - 1 , n) : INT_MAX ;
+        ll right = (m - 1 >= 0 && n - 1 >= 0 && n <= m) ? (ll) t[m][n] + f(t , m - 1 , n - 1) : INT_MAX ;
+
+        return memo[{m , n}] = min(top , right) ;
+    }
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size() ;
+        ll mn = INT_MAX ;
+        for(int i = 0 ; i < triangle[n - 1].size() ; i++){
+            mn = min(mn , f(triangle , n - 1 , i)) ;
+        }
+        return mn ;
+    }
+};
