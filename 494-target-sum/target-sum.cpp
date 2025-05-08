812b6865871d917b807e886589c36c347e24f895
Rudra <105686193+pandarudra@users.noreply.github.com>
2025-05-08 23:24:24 +0530

    Time: 272 ms (32.63%) | Memory: 61.3 MB (11.19%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,12 +1,17 @@
+#define ipair pair<int , int>
+
 class Solution {
 private:
+    map<ipair , int> dp ;
     int diffExp(vector<int>& nums , int n , int sum) {
         if(n < 0) return sum == 0 ;
 
+        if(dp.count({n , sum})) return dp[{n , sum}] ;
+        
         int pick = diffExp(nums , n - 1 , sum - nums[n]) ;
         int notpick = diffExp(nums , n - 1 , sum + nums[n]) ;
 
-        return pick + notpick ;
+        return dp[{n , sum}] = pick + notpick ;
     }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
