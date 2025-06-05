6179e8c3563f16483a9b21c36ad3d749ffee690b
Rudra <105686193+pandarudra@users.noreply.github.com>
2025-06-05 20:14:08 +0530

    Time: 23 ms (88.55%) | Memory: 26.8 MB (44.34%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,60 +1,66 @@
+#define all(v) (v).begin(),(v).end()
 class Solution {
 public:
     string shortestCommonSupersequence(string str1, string str2) {
-        int n = str1.length();
-        int m = str2.length();
-        
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-        vector<vector<char>> d(n + 1, vector<char>(m + 1, 'U')); // Corrected size
-        
-        // LCS Calculation
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (str1[i - 1] == str2[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                    d[i][j] = 'C'; // Diagonal move (match)
-                } else {
-                    if (dp[i - 1][j] >= dp[i][j - 1]) {
-                        dp[i][j] = dp[i - 1][j]; // Take from above
-                        d[i][j] = 'U';  // Up direction
-                    } else {
-                        dp[i][j] = dp[i][j - 1]; // Take from left
-                        d[i][j] = 'L';  // Left direction
-                    }
+          int n = str1.length() ;
+          int m = str2.length() ;
+
+          // Let's compute LCS table 1st 
+          vector<vector<int>> dp(n + 1 , vector<int>(m + 1 , -1)) ;
+
+          for(int i = 0 ; i <= n ; i++) dp[i][0] = 0 ;
+          for(int j = 0 ; j <= m ; j++) dp[0][j] = 0 ;
+
+          for(int i = 1 ; i <= n ; i++) {
+            for(int j = 1 ; j <= m ; j++) {
+                if(str1[i - 1] == str2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1] ;
+                }else {
+                    dp[i][j] = max(dp[i - 1][j] , dp[i][j - 1]) ;
                 }
             }
-        }
+          }
 
-        // Backtracking to build the shortest common supersequence
-        int i = n, j = m;
-        string res = "";
-        
-        while (i > 0 && j > 0) {
-            if (d[i][j] == 'C') {
-                res += str1[i - 1];
-                i--; j--; // Move diagonally
-            } else if (d[i][j] == 'U') {
-                res += str1[i - 1];
-                i--; // Move up
-            } else { // 'L'
-                res += str2[j - 1];
-                j--; // Move left
+          // table backtrack
+
+          string s = "" ;
+
+          int i = n , j = m ;
+          while(i > 0 && j > 0) {
+            if(str1[i - 1] == str2[j - 1]) {
+                s.push_back(str1[i - 1]) ;
+                i-- ;
+                j-- ;
+                continue ;
             }
-        }
+            int top = dp[i - 1][j] ;
+            int left = dp[i][j - 1] ;
 
-        // Add remaining characters
-        while (i > 0) {
-            res += str1[i - 1];
-            i--;
-        }
-        while (j > 0) {
-            res += str2[j - 1];
-            j--;
-        }
+            char topChar = str2[j - 1] ;
+            char leftChar = str1[i - 1] ;
 
-        // Reverse the result as we built it in reverse order
-        reverse(res.begin(), res.end());
+            if(top > left) {
+                s.push_back(leftChar) ;
+                i-- ;
+            }else {
+                s.push_back(topChar) ;
+                j-- ;
+            }
 
-        return res;
+          }
+
+          while(i > 0) {
+            s.push_back(str1[i - 1]) ;
+            i-- ;
+          }
+
+          while(j > 0) {
+            s.push_back(str2[j - 1]) ;
+            j-- ;
+          }
+
+          reverse(all(s)) ;
+
+          return s ;
     }
 };
