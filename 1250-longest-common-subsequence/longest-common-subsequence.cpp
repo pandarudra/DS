aac6991ab03cfd3fcfcb251da3a7932c51bd9a49
Rudra <105686193+pandarudra@users.noreply.github.com>
2025-06-05 07:54:44 +0530

    Time: 42 ms (25.79%) | Memory: 27.6 MB (14.69%) - LeetSync

---
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    int f(string& s1 , string& s2 , int n , int m , vector<vector<int>>& dp) {
+        if (m < 0 || n < 0) return 0 ;
+        if(dp[n][m] != -1) {
+            return dp[n][m] ;
+        }
+
+        // match 
+        if(s1[n] == s2[m]) {
+            return dp[n][m] = 1 + f(s1 , s2 , n - 1 , m - 1 , dp) ;
+        }
+        // not match
+        return dp[n][m] = max(f(s1 , s2 , n - 1 , m , dp) , f(s1 , s2 , n , m - 1 , dp)) ;
+    }
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size() ;
+        int m = text2.size() ;
+        
+        vector<vector<int>> dp(n , vector<int>(m , -1)) ;
+
+        return f(text1 , text2 , n - 1 , m - 1 , dp) ;
+    }
+};
