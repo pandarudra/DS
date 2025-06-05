6f4f96a48dfb3ab476522cc76769e95c5178e705
Rudra <105686193+pandarudra@users.noreply.github.com>
2025-06-05 08:05:39 +0530

    Time: 43 ms (23.28%) | Memory: 27.8 MB (6.26%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,13 +1,13 @@
 class Solution {
 private:
     int f(string& s1 , string& s2 , int n , int m , vector<vector<int>>& dp) {
-        if (m < 0 || n < 0) return 0 ;
+        if (m == 0 || n == 0) return 0 ;
         if(dp[n][m] != -1) {
             return dp[n][m] ;
         }
 
         // match 
-        if(s1[n] == s2[m]) {
+        if(s1[n - 1] == s2[m - 1]) {
             return dp[n][m] = 1 + f(s1 , s2 , n - 1 , m - 1 , dp) ;
         }
         // not match
@@ -18,8 +18,8 @@ public:
         int n = text1.size() ;
         int m = text2.size() ;
         
-        vector<vector<int>> dp(n , vector<int>(m , -1)) ;
+        vector<vector<int>> dp(n + 1 , vector<int>(m + 1 , -1)) ;
 
-        return f(text1 , text2 , n - 1 , m - 1 , dp) ;
+        return f(text1 , text2 , n , m , dp) ;
     }
 };
