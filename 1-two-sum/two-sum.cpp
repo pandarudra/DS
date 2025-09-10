a1101c769b28e6bc402eb87c1769d01f934872b6
Rudra <105686193+pandarudra@users.noreply.github.com>
2025-09-10 09:04:19 +0530

    Time: 8 ms (43.93%) | Memory: 15.8 MB (8.20%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,14 +1,30 @@
+#define umap unordered_map
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int N = nums.size() ;
+
+        // x + y = target
+        // y = target - x
+        // if i have x then i just have to find target - x
+        // for that i can keep a hashmap
+        // then for each x i have to check if target - x exists
+
+        umap<int , int> mp ;
+
         for(int i = 0 ; i < N ; i++) {
-            for(int j = i + 1 ; j < N ; j++) {
-                if(nums[i] + nums[j] == target) {
-                    return {i , j} ;
-                }
+            mp[nums[i]] = i ;
+        }
+
+
+        for(int i = 0 ; i < N ; i++) {
+            int x = nums[i] ;
+            int t_x = target - x ;
+            if(mp.count(t_x) && mp[t_x] != i) {
+                return {mp[t_x] , i} ;
             }
         }
+
         return {-1 , -1} ;
     }
 };
