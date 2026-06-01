cbdfb918701470a0b53608eda8476ec0897601f5
Rudra <rudrapanda8206@gmail.com>
2026-06-01 15:27:18 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (10.55%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,15 +1,20 @@
+#define pb push_back
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> dp(numRows) ;
-        dp[0] = {1} ;
-        for(int i = 1 ; i < numRows ; i++){
-            dp[i].resize(i + 1) ;
-            dp[i][0] = dp[i][i] = 1 ;
-            for(int j = 1 ; j < i ; j++){
-                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j] ;
+        vector<vector<int>> v(numRows) ;
+        // (2,1) => (1,0) + (1,1)
+        // (i,j) => (i - 1 , j - 1) + (i - 1 , j)
+        // j == 0 or i - 1 => 1
+        for(int i = 0 ; i < numRows ; i++) {
+            for(int j = 0 ; j <= i ; j++) {
+                if(j == 0 || j == i) {
+                    v[i].pb(1) ;
+                    continue ;
+                } 
+                v[i].pb(v[i - 1][j - 1] + v[i - 1][j]) ;
             }
         }
-        return dp ;
+        return v ;
     }
 };
