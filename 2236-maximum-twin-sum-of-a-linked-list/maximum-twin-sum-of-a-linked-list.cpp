86b9ccf62247cacfff7f6787bd967a445b893603
Rudra <rudrapanda8206@gmail.com>
2026-06-15 00:17:09 +0530

    Time: 2 ms (70.05%) | Memory: 131.4 MB (33.95%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
@@ -11,29 +11,25 @@
 #define all(v) (v).begin(),(v).end()
 using ln = ListNode* ;
 class Solution {
-    pair<ln , int> getmid(ln node) {
+    ln getmid(ln node) {
         ln slow = node ;
         ln fast = node ;
-        int len = 0 ;
+
         while(fast && fast->next) {
             if(slow->next) slow = slow->next ;
             if(fast->next) fast = fast->next ;
             if(fast->next) fast = fast->next ;
-            len++ ;
         }
-        return {slow , 2 * len} ;
+        return slow ;
     }
 public:
     int pairSum(ListNode* head) {
-        pair<ln , int> p = getmid(head) ;
-        int n = p.second ;
-        ln mid = p.first ;
+        ln mid = getmid(head) ;
         
         ln node = head ;
-
         stack<int> st ;
         
-        for(int i = 0 ; i < n / 2 ; i++) {
+        while(node != mid) {
             st.push(node->val) ;
             node = node->next ;
         }
