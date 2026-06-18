f8011482b28e9560a4f7272ef1b0bec3b7719725
Rudra <rudrapanda8206@gmail.com>
2026-06-18 23:42:00 +0530

    Time: 3 ms (26.29%) | Memory: 10.7 MB (39.84%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,20 +1,25 @@
-#define pb push_back
+#define umap unordered_map
 class Solution {
-    void getAllPermutes(vector<int>& nums , int i , vector<vector<int>>& v) {
-        if(i >= nums.size()) {
-            v.pb(nums) ;
-            return ;
-        }
-        for(int j = i ; j < nums.size() ; j++) {
-            swap(nums[i] , nums[j]) ;
-            getAllPermutes(nums , i + 1 , v) ;
-            swap(nums[i] , nums[j]) ;
-        }
+    void f(vector<int>& nums ,umap<int ,bool>& mp , vector<vector<int>>& ans , vector<int>& ds) {
+       if(ds.size() == nums.size()) {
+        ans.push_back(ds) ;
+        return ;
+       }
+       for(int i = 0 ; i < nums.size() ; i++) {
+         if(mp[i]) continue ;
+         mp[i] = true ;
+         ds.push_back(nums[i]) ;
+         f(nums , mp , ans , ds) ;
+         ds.pop_back() ;
+         mp[i] = false ;
+       }
     }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> v ;
-        getAllPermutes(nums , 0 , v) ;
-        return v ;
+        vector<vector<int>> ans ;
+        umap<int , bool> mp ;
+        vector<int> ds ;
+        f(nums , mp , ans , ds) ;
+        return ans ;
     }
 };
