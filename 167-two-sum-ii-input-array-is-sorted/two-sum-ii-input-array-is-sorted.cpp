af6fcd5518f49f2f4052d9a14067e5cc8651e147
Rudra <rudrapanda8206@gmail.com>
2026-07-27 09:55:22 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (65.28%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,21 +1,14 @@
 class Solution {
-    int getPos(vector<int>& v , int l , int h , int t) {
-        while(l <= h) {
-            int m = l + (h - l) / 2 ;
-            if(v[m] == t) return m ;
-            else if(v[m] < t) l = m + 1 ;
-            else h = m - 1 ;
-        }
-        return -1 ;
-    }
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         int n = numbers.size() ;
+        int left = 0 , right = n - 1 ;
 
-        for(int i = 0 ; i <= n - 1 ; i++) {
-            int x = i ;
-            int y = getPos(numbers , i + 1 , n - 1 , target - numbers[i]) ;
-            if(y != -1) return {x + 1 , y + 1} ;
+        while(left < right) {
+            int v = numbers[left] + numbers[right] ;
+            if(v == target) return {left + 1 , right + 1} ;
+            else if(v < target) left++ ;
+            else right-- ;
         }
 
         return {-1 , -1} ;
