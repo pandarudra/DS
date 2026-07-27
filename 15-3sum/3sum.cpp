2f7f8577e91c7f673085fc2cf3f3f6d0097190d0
Rudra <rudrapanda8206@gmail.com>
2026-07-27 22:55:17 +0530

    Time: 49 ms (49.77%) | Memory: 29.8 MB (27.15%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,36 +1,33 @@
-#define umap unordered_map
-#define all(S) (S).begin(),(S).end()
-#define pb push_back
+#define all(v) (v).begin(),(v).end()
 class Solution {
+    void twoSum(vector<vector<int>>& ans , vector<int>& v , int left , int t) {
+        int right = v.size() - 1 ;
+        while(left < right) {
+            int sum = v[left] + v[right] ;
+            if(sum == t) {
+                ans.push_back({-t , v[left] , v[right]}) ;
+                left++ ;
+                right-- ;
+                 while(left < v.size() && v[left] == v[left - 1]) left++ ;
+                 while(right >= 0 && v[right] == v[right + 1]) right-- ;
+            }
+            else if(sum < t) left++ ;
+            else right-- ;
+        }
+    }
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n = nums.size() ;
         sort(all(nums)) ;
+
+        if(nums[0] > 0) return {} ;
+
         vector<vector<int>> ans ;
         for(int i = 0 ; i < n ; i++) {
-
             if(i > 0 && nums[i] == nums[i - 1]) continue ;
 
-            int sum = -nums[i] ;
-            int left = i + 1 , right = n - 1 ;
-
-            while(left < right) {
-                int theta = nums[left] + nums[right] ;
-                if(theta == sum) {
-                    ans.pb({nums[i] , nums[left] , nums[right]}) ;
-                    left++ ;
-                    right-- ;
-
-                    while(left < right &&  nums[left] == nums[left - 1]) left++ ;
-
-                    while(left < right && nums[right] == nums[right + 1]) right-- ;
-                    
-                } else if(theta > sum) {
-                    right-- ;
-                } else {
-                    left++ ;
-                }
-            }
+            int x = nums[i] ;
+            twoSum(ans , nums , i + 1 , -x) ;
         }
         return ans ;
     }
