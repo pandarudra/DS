ee25273850e466884541562fff798d16a308ca8a
Rudra <rudrapanda8206@gmail.com>
2026-08-19 00:58:35 +0530

    Time: 0 ms (100.00%) | Memory: 29.1 MB (78.95%) - LeetSync

--- 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.cpp
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.cpp
@@ -1,9 +1,7 @@
 #define all(v) (v).begin(),(v).end()
 class Solution {
-    int k1(vector<int>& nums) {
+    int k1(vector<int>& nums , int freq[]) {
         int mx = -1 ;
-        int freq[51] = {0} ;
-        for(auto x : nums) freq[x]++ ;
         for(auto x : nums) {
             if(freq[x] == 1) {
                 mx = max(mx , x) ;
@@ -11,27 +9,22 @@ class Solution {
         }
         return mx ;
     }
-    int k2n(vector<int>& nums) {
+    int k2n(vector<int>& nums , int freq[]) {
         int n = nums.size() ;
         int mx = -1 ;
-        int freq[51] = {0} ;
-        for(auto x : nums) freq[x]++ ;
-
-
-
         int a = nums[0] , b = nums[n - 1] ;
         mx = freq[max(a , b)] == 1 ? max(a , b) : min(a , b) ;
-
         if(freq[mx] != 1) mx = -1 ;
-
         return mx  ;
     }    
 public:
     int largestInteger(vector<int>& nums, int k) {
         int n = nums.size() ;
+        int freq[51] = {0} ;
+        for(auto x : nums) freq[x]++ ;
         if(k == n) return *max_element(all(nums)) ;
-        else if(k == 1) return k1(nums) ;
-        else return k2n(nums) ;
+        else if(k == 1) return k1(nums , freq) ;
+        else return k2n(nums , freq) ;
         return -1 ;
     }
 };
