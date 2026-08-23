cbb1c3384d5c07c61ffa88ecbcecd583c08dc6da
Rudra <rudrapanda8206@gmail.com>
2026-08-24 01:22:06 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (70.80%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -1,38 +1,44 @@
 class Solution {
+    ListNode* reverse(ListNode* node) {
+        ListNode* prev = nullptr ;
+        ListNode* current = node ;
+        while(current != nullptr) {
+            ListNode* cn = current->next ;
+            current->next = prev ;
+            prev = current ;
+            current = cn ;
+        }
+        return prev ;
+    }
+    ListNode* getKthNode(ListNode* node , int k) {
+        while(node != nullptr && --k) node = node->next ;
+        if(k) return nullptr ;
+        return node ;
+    }
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
-        deque<ListNode*> dq ;
         ListNode* dummy = new ListNode(-1) ;
 
         ListNode* node = head ;
+        dummy->next = head ;
         ListNode* dn = dummy ;
+
+
         while(node != nullptr) {
-            if(dq.size() == k) {
-                while(!dq.empty()) {
-                    dn->next = dq.back() ;
-                    dn = dn->next ;
-                    dq.pop_back() ;
-                }
+            ListNode* kn = getKthNode(node , k) ;
+
+            if(kn == nullptr) {
+                break ;
             }
-            dq.push_back(node) ;
-            node = node->next ;
+            
+            ListNode* knn = kn->next ;
+            kn->next = nullptr ;
+            dn->next = reverse(node) ;
+            node->next = knn ;
+            dn = node ;
+            node = knn ;
         }
 
-        if(dq.size() == k) {
-            while(!dq.empty()) {
-                dn->next = dq.back() ;
-                dq.pop_back() ;
-                dn = dn->next ;
-            }
-        }
-
-
-        while(!dq.empty()) {
-            dn->next = dq.front() ;
-            dq.pop_front() ;
-            dn = dn->next ;
-        }
-        dn->next = nullptr ;
         return dummy->next ;
     }
 };
