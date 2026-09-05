7b24539624ccb1fb68407729f131585b6390c152
Rudra <rudrapanda8206@gmail.com>
2026-09-05 17:07:51 +0530

    Time: 1374 ms (46.85%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,4 +1,3 @@
-SELECT euni.unique_id , e.name
-FROM Employees e 
-LEFT JOIN EmployeeUNI euni
-ON e.id = euni.id
+SELECT en.unique_id , e.name from 
+Employees e left join EmployeeUNI en
+ON e.id = en.id 
