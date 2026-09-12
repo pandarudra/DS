635dbda0ebe501cd203a3389fd4ff12dcf177f9d
Rudra <rudrapanda8206@gmail.com>
2026-09-12 12:04:55 +0530

    Time: 4 ms (37.64%) | Memory: 28.7 MB (38.36%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -1,44 +1,44 @@
 class Solution {
-private:
-    int drow[4] = {0 , 0 , -1 , 1} ;
-    int dcol[4] = {-1 , 1 , 0 , 0} ;
+    vector<int> dr = {-1 , 0 , 1 , 0} ;
+    vector<int> dc = {0 , 1 , 0 , -1} ;
     
-    void bfs(int r , int c , vector<vector<int>>& grid , vector<vector<int>>& vis, int &mxArea){
-        int  n = grid.size() ;
-        int  m = grid[0].size() ;
+    void mark(int r , int c , int& area , vector<vector<int>>& vis , vector<vector<int>>& grid) {
+        int m = grid.size() ;
+        int n = grid[0].size() ;
+
+        if(r < 0 || r >= m) return ;
+        if(c < 0 || c >= n) return ;
+
+        if(vis[r][c]) return ;
+        if(grid[r][c] == 0) return ;
+
+        area++ ;
         vis[r][c] = 1 ;
-        queue<pair<int,int>> q ;
-        q.push({r , c}) ;
-        int area = 0 ;
-        while(!q.empty()){
-            int row = q.front().first ;
-            int  col = q.front().second ;
-            q.pop() ;
-            area += grid[row][col] ;
-            for(int i = 0 ; i < 4 ; i++){
-                int nr = row + drow[i] ;
-                int nc = col + dcol[i] ;
-                if(nr >= 0 && nr < n && nc >= 0 && nc < m && !vis[nr][nc] && grid[nr][nc] > 0){
-                    vis[nr][nc] = 1 ;
-                    q.push({nr , nc}) ;
-                }
-            }
+
+        for(int i = 0 ; i < 4 ; i++) {
+            int nr = r + dr[i] ;
+            int nc = c + dc[i] ;
+
+            mark(nr , nc , area , vis , grid) ;
         }
-        mxArea = max(mxArea , area) ;
     }
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int n = grid.size() ;
-        int m = grid[0].size() ;
-        vector<vector<int>> vis(n , vector<int>(m , 0)) ;
-        int mxArea = 0 ;
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                if(grid[i][j] > 0 && !vis[i][j]){
-                    bfs(i , j , grid , vis , mxArea) ;
+        int m = grid.size() ;
+        int n = grid[0].size() ;
+
+        vector<vector<int>> vis(m , vector<int>(n , 0)) ;
+        int mx = 0 ;
+
+        for(int i = 0 ; i < m ; i++) {
+            for(int j = 0 ; j < n ; j++) {
+                if(grid[i][j] && !vis[i][j]) {
+                    int area = 0 ;
+                    mark(i , j , area , vis , grid) ;
+                    mx = max(area , mx) ;
                 }
             }
         }
-        return mxArea ;
+        return mx ;
     }
 };
