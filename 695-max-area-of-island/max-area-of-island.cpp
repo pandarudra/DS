8b5f3e20bbf4123b708647f12e2337c82748daf6
Rudra <rudrapanda8206@gmail.com>
2026-09-12 12:21:52 +0530

    Time: 0 ms (100.00%) | Memory: 28.5 MB (42.99%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -2,25 +2,27 @@ class Solution {
     vector<int> dr = {-1 , 0 , 1 , 0} ;
     vector<int> dc = {0 , 1 , 0 , -1} ;
     
-    void mark(int r , int c , int& area , vector<vector<int>>& vis , vector<vector<int>>& grid) {
+    int mark(int r , int c , vector<vector<int>>& vis , vector<vector<int>>& grid) {
         int m = grid.size() ;
         int n = grid[0].size() ;
 
-        if(r < 0 || r >= m) return ;
-        if(c < 0 || c >= n) return ;
+        if(r < 0 || r >= m) return 0 ;
+        if(c < 0 || c >= n) return 0 ;
 
-        if(vis[r][c]) return ;
-        if(grid[r][c] == 0) return ;
+        if(vis[r][c]) return 0 ;
+        if(grid[r][c] == 0) return 0 ;
 
-        area++ ;
         vis[r][c] = 1 ;
+        int area = 1 ;
 
         for(int i = 0 ; i < 4 ; i++) {
             int nr = r + dr[i] ;
             int nc = c + dc[i] ;
 
-            mark(nr , nc , area , vis , grid) ;
+            area += mark(nr , nc , vis , grid) ;
         }
+
+        return area ;
     }
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
@@ -33,8 +35,7 @@ public:
         for(int i = 0 ; i < m ; i++) {
             for(int j = 0 ; j < n ; j++) {
                 if(grid[i][j] && !vis[i][j]) {
-                    int area = 0 ;
-                    mark(i , j , area , vis , grid) ;
+                    int area = mark(i , j , vis , grid) ;
                     mx = max(area , mx) ;
                 }
             }
