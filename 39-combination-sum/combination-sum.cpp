c285fcf949771541a354251152df3995ae4d27a9
Rudra <rudrapanda8206@gmail.com>
2026-09-15 16:47:38 +0530

    Time: 4 ms (36.35%) | Memory: 14.1 MB (76.30%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,27 +1,39 @@
+#define all(vect) begin(vect),end(vect)
 class Solution {
-    void f(vector<int>& c , vector<vector<int>>& ans , vector<int>& v , int t , int i) {
-        if(t < 0) return ;
-        if(t == 0) {
-            ans.push_back(v) ;
-            return ;
-        }
-        if(i >= c.size()) {
+    void f(vector<int>& a , vector<int>& ds , int i , int t , vector<vector<int>>& ans) {
+        int n = a.size() ;
+        if(i >= n) {
             if(t == 0) {
-                ans.push_back(v) ;
+                ans.push_back(ds) ;
             }
             return ;
         }
 
-        v.push_back(c[i]) ;
-        f(c , ans , v , t - c[i] , i) ;
-        v.pop_back() ;
-        f(c , ans , v, t , i + 1) ;
+        if(t == 0) {
+            ans.push_back(ds) ;
+            return ;
+        }
+
+        if(t < 0) return ;
+
+        while(i > 0 && a[i] == a[i - 1]) i++ ;
+
+        // take 
+        ds.push_back(a[i]) ;
+        f(a , ds , i , t - a[i] , ans) ;
+        ds.pop_back() ;
+
+
+        // not take 
+        f(a , ds , i + 1 , t , ans) ;
     }
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        int n = candidates.size() ;
+        sort(all(candidates)) ;
         vector<vector<int>> ans ;
-        vector<int> v ;
-        f(candidates , ans , v , target , 0) ;
+        vector<int> ds ;
+        f(candidates , ds , 0 , target , ans) ;
         return ans ;
     }
 };
